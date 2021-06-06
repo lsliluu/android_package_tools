7951523391eb64c8b44134838233bb7d040f0c4f
dev.hezf@gmail.com <MM19941228>
2021-06-06 21:51:00 +0800

    打包初步

--- Utils/PackageUtils.py
+++ Utils/PackageUtils.py
@@ -1,10 +1,31 @@
+from flask import g
+
+
 def execute_doc_shell():
     pass
 
 
 def execute_shell():
+    # 执行脚本
+    from pip._internal import commands
+    (status, output) = commands.getstatusoutput('sh hello.sh')
+    # 调用发送邮件
+    send_email(status, output)
     pass
 
 
-def send_email():
+def send_email(status, output):
+    if status == 0:
+        # 获取成功邮件模板
+
+        # 获取apk附件
+        pass
+    else:
+        # 获取失败邮件模板
+
+        # 将output输出成日志文件，放到邮件附件中发送
+
+        pass
+    # 释放全局变量
+    g.packaging_sign = False
     pass
